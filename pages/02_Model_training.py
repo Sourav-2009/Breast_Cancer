57473dbdc253097d20af5bc830b840ec0bffb638
Salem Shaikh <slmsshk21@gmail.com>
2023-06-14 22:52:12 +0530

    Update 02_Model_training.py

---
+++ pages/02_Model_training.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import time
+
+st.markdown("""<h1 style="font-size:50px:padding:1500px;background-color:white;color:green">Under Construction 🚧🏗</h1>""",unsafe_allow_html=True)
+
+while True:
+    st.snow()
+    time.sleep(15)
